b924f6e244a78f93567e6e04c969787c75289b51
zain <zainirfan565@gmail.com>
2024-06-09 17:04:14 +0500

    Mern stack crud book app using graphql

---
+++ apollo-server/src/index.js
@@ -0,0 +1,35 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers } from "./resolvers.js";
+import { typeDefs } from "./models/typeDefs.js";
+import mongoose from "mongoose";
+// GraphQL Schema
+// const typeDefs = gql`
+//   type Query {
+//     hello(name: String): String
+//   }
+// `;
+
+// // GraphQL Resolvers
+// const resolvers = {
+//   Query: {
+//     hello: (_, { name }) => `Hello ${name}`,
+//   },
+// };
+// const HELLO_QUERY = gql`
+//   query Query($name: String) {
+//     hello(name: $name)
+//   }
+// `;
+const server = new ApolloServer({ typeDefs, resolvers });
+mongoose.set("strictQuery", true);
+const db = await mongoose.connect("mongodb://localhost:27017/mern", {
+  useNewUrlParser: true,
+});
+console.info("📚 Connected to db", db?.connections[0]?._connectionString);
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.info(`🚀 Server ready at ${url}`);
---
+++ apollo-server/src/models/typeDefs.js
@@ -0,0 +1,23 @@
+import gql from "graphql-tag";
+
+// export const typeDefs = gql`
+//   type Query {
+//     hello(name: String): String
+//   }
+// `;
+export const typeDefs = gql`
+  type Query {
+    hello(name: String): String
+    books: [Book]
+  }
+  type Book {
+    id: ID
+    title: String
+    year: Int
+  }
+  type Mutation {
+    create(title: String, year: Int): Book
+    delete(id: ID): ID
+    edit(id: ID, title: String, year: Int): Book
+  }
+`;
